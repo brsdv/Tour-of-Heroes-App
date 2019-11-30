c85922f9a8b32ad305d4a0490d0586fa70a5ea30
Boris Davydenko <brsdv@yandex.ru>
2019-11-30 16:55:40 +0300

    create messagesService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { HeroService } from './services/hero.service';
 import { MessagesComponent } from './components/messages/messages.component';
+import { HeroService } from './services/hero.service';
+import { MessagesService } from './services/messages.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MessagesComponent } from './components/messages/messages.component';
     FormsModule
   ],
   providers: [
-    HeroService
+    HeroService,
+    MessagesService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Hero } from '../models/hero';
 import { HEROES } from '../mocks/mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messagesService: MessagesService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messagesService.add('HeroService: Избранные герои');
     return of(HEROES);
   }
 }
